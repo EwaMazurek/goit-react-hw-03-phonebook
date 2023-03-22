8ebddca76bacaf07bf3a4c6495ecd7b9bcc4447e
EwaMazurek <ewa.mmazurek@gmail.com>
2023-03-22 16:05:39 +0100

    task complete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,13 +5,9 @@ import ContactList from './contacts/ContactList';
 import ContactsFilter from './filter/ContactsFilter';
 
 export class App extends Component {
+  savedState = localStorage.getItem('contacts');
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: this.savedState === null ? [] : JSON.parse(this.savedState),
     filter: '',
   };
 
@@ -40,6 +36,8 @@ export class App extends Component {
       this.setState(prevState => ({
         contacts: [...prevState.contacts, newContact],
       }));
+      const array = [...this.state.contacts, newContact];
+      localStorage.setItem('contacts', JSON.stringify(array));
     }
   };
 
